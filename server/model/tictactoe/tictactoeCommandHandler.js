9845b0d1b8534033863437802ad4f45309320010
agirmar92 <aegir13@ru.is>
2015-12-08 14:53:42 +0000

    should create game with another user another time - passed

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -2,7 +2,13 @@ var _ = require('lodash');
 module.exports = function tictactoeCommandHandler(events) {
   return {
     executeCommand: function(cmd) {
-      return [{"id":"987","event":"GameCreated","userName":"Agirmar","timeStamp":"2015.05.07T09:15:22","name":"InitialGame"}];
+      return [{
+        id: cmd.id,
+        event: "GameCreated",
+        userName: cmd.userName,
+        timeStamp: cmd.timeStamp,
+        name: cmd.name
+      }];
     }
   }
 };
