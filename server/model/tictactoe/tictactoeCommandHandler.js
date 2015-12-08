2c20b778db075591283af0504a97f780eb194be2
agirmar92 <aegir13@ru.is>
2015-12-08 17:23:09 +0000

    should make a move and X wins - passed

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -6,11 +6,44 @@ module.exports = function tictactoeCommandHandler(events) {
     board: [['','',''],['','',''],['','','']]
   };
 
+  var makeMove = function(x, y, side) {
+    gameState.board[x][y] = side;
+  }
+
   _.each(events, function(event){
     if (event.event === "MoveMade") {
-      gameState.board[event.x][event.y] = event.side;
+      makeMove(event.x, event.y, event.side);
     }
   });
+
+  // Returns 0 if the game is not finished
+  // Returns 1 if the player that just made a move has won
+  // Returns 2 if the game finished a draw
+  var isFinished = function(x, y, side) {
+    // Check for diagonal win
+    if (gameState.board[0][0] === side && gameState.board[1][1] === side && gameState.board[2][2] === side) {
+      return 1;
+    } 
+    if (gameState.board[2][0] === side && gameState.board[1][1] === side && gameState.board[0][2] === side) {
+      return 1;
+    }
+    // Check for row win
+    if (gameState.board[x][0] === side && gameState.board[x][1] === side && gameState.board[x][2] === side) {
+      return 1;
+    }
+    // Check for column win
+    if (gameState.board[0][y] === side && gameState.board[1][y] === side && gameState.board[2][y] === side) {
+      return 1;
+    }
+
+    for (var i = 0; i < 3; i++) {
+      for (var k = 0; k < 3; k++) {
+        if (gameState.board[i][k] === '')
+          return 0;
+      }
+    }
+    return 2;
+  }
 
   return {
     executeCommand: function(cmd) {
@@ -31,11 +64,21 @@ module.exports = function tictactoeCommandHandler(events) {
         } else if (gameState.board[cmd.x][cmd.y] !== '') {
           reply[0].event = "IllegalMove (move already made)";
         } else {
-          reply[0].event = "MoveMade";
+          makeMove(cmd.x, cmd.y, cmd.side);
+          var finished = isFinished(cmd.x, cmd.y, cmd.side);
+
+          if (finished === 0) {
+            reply[0].event = "MoveMade";
+          } else if (finished === 1) {
+            reply[0].event = "WinningMoveMade";
+          } else if (finished === 2) {
+            reply[0].event = "Derksadasd";
+          }
         }
         
         return reply;
       }
+
       if (cmd.comm === "CreateGame") {
         return [{
           id: cmd.id,
