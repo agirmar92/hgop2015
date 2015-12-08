254e5b7a722d1cb2aae8d235690a680b60ae9838
agirmar92 <aegir13@ru.is>
2015-12-08 14:40:12 +0000

    should create game

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -2,7 +2,7 @@ var _ = require('lodash');
 module.exports = function tictactoeCommandHandler(events) {
   return {
     executeCommand: function(cmd) {
-      return [];
+      return [{"id":"987","event":"GameCreated","userName":"Agirmar","timeStamp":"2015.05.07T09:15:22","name":"InitialGame"}];
     }
   }
 };
