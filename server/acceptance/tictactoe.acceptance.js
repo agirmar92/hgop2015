7ce262bf2fa76481bd71ed7c4da9a96839d7d29e
agirmar92 <aegir13@ru.is>
2015-12-13 03:15:39 +0000

    now fluid-api acceptance test is ready

--- server/acceptance/tictactoe.acceptance.js
+++ server/acceptance/tictactoe.acceptance.js
@@ -51,25 +51,40 @@ describe('TEST ENV GET /api/gameHistory', function () {
   });
 
 	it('Should execute fluid API test - same as above', function (done) {
-		given(user("Siggi").createsGame("ElitesOnly").withId("555")).sendTo("/api/createGame").expect(200).and(/json/).when(done);
+		given(user("Siggi").createsGame("ElitesOnly").withId("555")).expect("GameCreated").withName("ElitesOnly").isOk(done);
+	});
+
+	it('Should play a game until drawn', function (done) {
+		/*var player1 = "John";
+		var player2 = "Matthew";
+
+		given(
+			user(player1).createsGame("I am undefeated").withId("12")
+			.and(player2).joinGame("12")
+			.and(player1).makesMove(1,1)
+			.and(player2).makesMove(2,0)
+			.and(player1).makesMove(0,1)
+			.and(player2).makesMove(0,0)
+			.and(player1).makesMove(1,0)
+			.and(player2).makesMove(2,1)
+			.and(player1).makesMove(2,2)
+			.and(player2).makesMove(1,2)
+		).sendTo("/api/createGame")
+		.expect();*/
 	});
 
 	var given = function(command) {
 
-		console.log(command);
-
 		var cmd = command;
 		var expectations = [];
-		var destination;
 		
 		var givenApi = {
-			expect: function(event) {
-				expectations.push(event);
-				return givenApi;
-			},
+			expect: function(eventValue) {
+				expectations.push({
+					key: "event",
+					value: eventValue
+				});
 
-			sendTo: function(dest) {
-				destination = dest;
 				return givenApi;
 			},
 
@@ -77,35 +92,36 @@ describe('TEST ENV GET /api/gameHistory', function () {
 				return givenApi.expect(event);
 			},
 
-			when: function(done) {
+			withName: function(name) {
+				expectations.push({
+					key: "name",
+					value: name
+				});
+
+				return givenApi;
+			},
+
+			isOk: function(done) {
 				// Logic goes here
 				var req = request(acceptanceUrl);
 				req
-					.post(destination)
+					.post(command.destination)
 					.type('json')
-					.send(command)
-					.end(function (err, res) {
+					.send(command.data)
+					.end(function(err, res) {
 						if (err) return done(err);
-						request(acceptanceUrl)
-						.get('/api/gameHistory/555')
-						.expect(expectations[0])
-						.expect('Content-Type', expectations[1])
-						.end(function (err, res) {
-							if (err) return done(err);
-							res.body.should.be.instanceof(Array);
-							should(res.body).eql(
-							[{
-								"id": "1234",
-								"gameId": "555",
-								"event": "GameCreated",
-								"userName": "Siggi",
-								"name": "ElitesOnly",
-								"timeStamp": "2015-12-02T11:11:29"
-							}]);
-							done();
-						});
+						for (var i = 0; i < expectations.length; i++) {
+							var currKey = expectations[i].key;
+							var currValue = expectations[i].value;
+
+							if (currKey === "event")
+								should(res.body[0].event).eql(currValue);
+							else if (currKey === "name")
+								should(res.body[0].name).eql(currValue);
+						}
+
+						done();
 					});
-				done();
 			}
 		}
 
@@ -116,18 +132,26 @@ describe('TEST ENV GET /api/gameHistory', function () {
 
 		var userApi = {
 			createsGame: function(gameName) {
+
+				var createCommand = {
+					data: {
+						id: "1234",
+						gameId: "",
+						comm: "CreateGame",
+						userName: userName,
+						name: gameName,
+						timeStamp: "2015-12-02T11:11:29"
+					},
+					destination: "/api/createGame"
+				}
+
 				var createGameApi = {
 					withId: function(id) {
-						return {
-							id: "1234",
-							gameId: id,
-							comm: "CreateGame",
-							userName: userName,
-							name: gameName,
-							timeStamp: "2015-12-02T11:11:29"
-						};
+						createCommand.data.gameId = id;
+						return createCommand;
 					}
 				};
+
 				return createGameApi;
 			}
 		};
