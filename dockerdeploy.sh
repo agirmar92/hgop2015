db2302f078cd200d7e36545fc60093751f7e9442
agirmar92 <aegir13@ru.is>
2015-12-14 23:56:19 +0000

    fixing deploy script

--- dockerdeploy.sh
+++ dockerdeploy.sh
@@ -5,7 +5,7 @@ if [ $# != 5 ]; then
 	exit
 fi
 
-echo "Deploying revision '$2' of '$1'/tictactoe onto '$3' '$4':'$5'"
+echo "Deploying revision '$2' of $1/tictactoe onto $3 $4:$5"
 echo "______________________________________"
 
 echo \(TestMachine\) Stopping and removing old processes
@@ -14,10 +14,10 @@ ssh "$3" 'docker rm $(docker ps -a -q)'
 echo "______________________________________"
 
 echo \(TestMachine\) Pulling docker image
-ssh "$3" 'docker pull '$1'/tictactoe:'$2''
+ssh "$3" 'docker pull $1/tictactoe:$2'
 echo "______________________________________"
 
 echo \(TestMachine\) Starting the new image
-ssh "$3" 'docker run -p '$4:$5' -d -e "NODE_ENV=production" '$1'/tictactoe'$2''
+ssh "$3" 'docker run -p $4:$5 -d -e "NODE_ENV=production" $1/tictactoe:$2'
 echo "______________________________________"
 
