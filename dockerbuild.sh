c2945d3272ff4d764cd50f2149235aec6a220c3f
agirmar92 <aegir13@ru.is>
2015-12-14 21:22:48 +0000

    script changes, day 10

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -9,6 +9,14 @@ fi
 echo Cleaning...
 rm -rf ./dist
 
+if [ -z "$GIT_COMMIT" ]; then
+  export GIT_COMMIT=$(git rev-parse HEAD)
+  export GIT_URL=$(git config --get remote.origin.url)
+fi
+
+# Remove .git from url in order to get https link to repo (assumes https url for GitHub)
+export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
+
 echo Building app
 npm install
 bower install
@@ -19,6 +27,23 @@ if [[ $rc != 0 ]] ; then
     echo "Grunt build failed with exit code " $rc
     exit $rc
 fi
+
+cat > ./dist/githash.txt <<_EOF_
+$GIT_COMMIT
+_EOF_
+
+cat > ./dist/public/version.html << _EOF_
+<!doctype html>
+<head>
+   <title>TicTacToe version information</title>
+</head>
+<body>
+   <span>Origin:</span> <span>$GITHUB_URL</span>
+   <span>Revision:</span> <span>$GIT_COMMIT</span>
+   <p>
+   <div><a href="$GITHUB_URL/commits/$GIT_COMMIT">History of current version</a></div>
+</body>
+_EOF_
 
 cp ./Dockerfile ./dist/
 
@@ -39,10 +64,13 @@ if [[ $rc != 0 ]] ; then
     exit $rc
 fi
 
-echo "______________________________________"
-
 echo Pushing docker image
-docker push "$1"/tictactoe
+docker push "$1"/tictactoe:$GIT_COMMIT .
+rc=$?
+if [[ $rc != 0 ]] ; then
+    echo "Docker push failed " $rc
+    exit $rc
+fi
 
 
 echo "Done"
--- dockerdeploy.sh
+++ dockerdeploy.sh
@@ -1,22 +1,23 @@
 #!/bin/bash -e
 
-if [ $# != 4 ]; then
-	echo "Usage: ./dockerdeploy.sh <docker username> <ip address> <port in> <port out>"
+if [ $# != 5 ]; then
+	echo "Usage: ./dockerdeploy.sh <docker username> <git revision> <ip address> <port in> <port out>"
 	exit
 fi
 
+echo "Deploying revision '$2' of '$1'/tictactoe onto '$3' '$4':'$5'"
 echo "______________________________________"
 
 echo \(TestMachine\) Stopping and removing old processes
-ssh "$2" 'docker stop $(docker ps -a -q)'
-ssh "$2" 'docker rm $(docker ps -a -q)'
+ssh "$3" 'docker stop $(docker ps -a -q)'
+ssh "$3" 'docker rm $(docker ps -a -q)'
 echo "______________________________________"
 
 echo \(TestMachine\) Pulling docker image
-ssh "$2" 'docker pull '$1'/tictactoe'
+ssh "$3" 'docker pull '$1'/tictactoe:'$2''
 echo "______________________________________"
 
 echo \(TestMachine\) Starting the new image
-ssh "$2" 'docker run -p '$3:$4' -d -e "NODE_ENV=production" '$1'/tictactoe'
+ssh "$3" 'docker run -p '$4:$5' -d -e "NODE_ENV=production" '$1'/tictactoe'$2''
 echo "______________________________________"
 
