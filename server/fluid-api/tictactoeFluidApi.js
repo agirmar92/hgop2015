8b631e2a25ceabb43f0ffd01954f9c941b51b269
agirmar92 <aegir13@ru.is>
2015-12-14 15:14:35 +0000

    fluid-api acceptance tests now passing

--- server/fluid-api/tictactoeFluidApi.js
+++ server/fluid-api/tictactoeFluidApi.js
@@ -4,7 +4,6 @@ var acceptanceUrl = process.env.ACCEPTANCE_URL;
 
 var given = function(commands) {
 
-	//console.log(commands);
 
 	var currEvent = 0;
 	var currExpect = 0;
@@ -13,8 +12,8 @@ var given = function(commands) {
 
 	var expectations = [{
 		id: cmd.id,
-		gameId: cmd.gameId,
 		event: "",
+		gameId: cmd.gameId,
 		userName: cmd.userName,
 		name: cmd.name,
 		timeStamp: cmd.timeStamp
@@ -22,9 +21,8 @@ var given = function(commands) {
 
 	var executeCommand = function(done, callback) {
 		// Logic goes here
+		cmd = commands.returnValue[currEvent].data;
 		var req = request(acceptanceUrl);
-		//console.log("about to execute:");
-		//console.log(cmd);
 		req
 			.post(commands.returnValue[currEvent].destination)
 			.type('json')
@@ -36,7 +34,7 @@ var given = function(commands) {
 
 				if (currEvent !== commands.returnValue.length) {
 					// Execute next command
-					callback(done);
+					callback(done, callback);
 				} else {
 					// Modifying the expectations, to something that should be the same as the command
 					expectations[currExpect].timeStamp = cmd.timeStamp;
@@ -68,14 +66,25 @@ var given = function(commands) {
 			return givenApi;
 		},
 
+		withCoordinates: function(x, y) {
+			expectations[currExpect].x = x;
+			expectations[currExpect].y = y;
+			return givenApi;
+		},
+
 		withOtherUser: function(username) {
 			expectations[currExpect].otherUserName = username;
 			return givenApi;
 		},
 
+		withSymbol: function(symbol) {
+			expectations[currExpect].side = symbol;
+			return givenApi;
+		},
+
 		isOk: function(done) {
-			cmd = commands.returnValue[currEvent].data;
-			executeCommand(done, this.isOk);
+			// Execute every command, with help of callbacks
+			executeCommand(done, executeCommand);
 		}
 	}
 
@@ -87,11 +96,14 @@ var user = function(userName) {
 	var command  = {};
 	var commands = [];
 
+	var currUser = userName;
+
 	var userApi = {
 		returnValue: [],
 
 		and: function(userName) {
 			command = {};
+			currUser = userName;
 			return userApi;
 		},
 
@@ -101,7 +113,7 @@ var user = function(userName) {
 					id: "1234",
 					gameId: "",
 					comm: "CreateGame",
-					userName: userName,
+					userName: currUser,
 					name: gameName,
 					timeStamp: "2015-12-02T11:11:29"
 				},
@@ -117,7 +129,7 @@ var user = function(userName) {
 					id: "9876",
 					comm: "JoinGame",
 					gameId: "",
-					userName: userName,
+					userName: currUser,
 					name: gameName,
 					timeStamp: "2015.05.07T09:17:35"
 				},
@@ -127,13 +139,36 @@ var user = function(userName) {
 			return userApi;
 		},
 
-		makesMove: function(x, y) {
-			//
+		makesMove: function(xPos, yPos, symbol) {
+			var theId = commands[0].data.gameId;
+			var theName = commands[0].data.name;
+
+			command = {
+				data: {
+					id: "12345",
+			        comm: "MakeMove",
+			        gameId: theId,
+			        userName: currUser,
+			        name: theName,
+			        x: xPos,
+			        y: yPos,
+			        side: symbol,
+			        timeStamp: "2015.05.07T09:20:42"
+				},
+				destination: "/api/placeMove"
+			};
+
+
+			commands.push(command);
+			this.returnValue = commands;
+
 			return userApi;
 		},
 
 		withId: function(id) {
 			command.data.gameId = id;
+
+
 			commands.push(command);
 			this.returnValue = commands;
 
